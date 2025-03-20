de9343a745201b5e96493cbe64803bf2a36d3627
korbash <korbash.sen@gmail.com>
2025-03-20 14:33:12 +0300

    add get settings

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { DefaultService, SettingsOutput } from '../../api';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css'
 })
-export class SettingsComponent {
-  constructor() {}
+export class SettingsComponent implements OnInit {
+  setings_str = "";
+  constructor(private apiService: DefaultService) { }
+
+  ngOnInit() {
+    this.loadSettings();
+  }
+
+  private loadSettings() {
+    this.apiService.getSettingsSettingsGet().subscribe((data: SettingsOutput) => {
+      this.setings_str = JSON.stringify(data, null, 2);
+    });
+  }
 }
