660d1e22754b4fd5bb38834acb7dbc2fb74f5ae1
korbash <korbash.sen@gmail.com>
2025-03-23 11:16:29 +0300

    setup autocomplit keymap

--- src/app/pages/query/query.component.ts
+++ src/app/pages/query/query.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CodeEditor, Setup } from '@acrodata/code-editor';
 import { languages } from '@codemirror/language-data';
+import { keymap, EditorView } from '@codemirror/view';
+import { Prec } from '@codemirror/state';
+import { indentMore, indentLess, indentWithTab } from '@codemirror/commands';
+import { acceptCompletion, completionStatus, autocompletion, closeCompletion, moveCompletionSelection } from '@codemirror/autocomplete';
+import { basicSetup } from 'codemirror';
 
 @Component({
   selector: 'app-query',
@@ -12,6 +17,19 @@ import { languages } from '@codemirror/language-data';
 export class QueryComponent {
   value = 'SELECT * FROM table';
   language = "SQL";
-  setup: Setup = "basic"
   languages = languages;
+
+  // Кастомная конфигурация с сохранением встроенного автодополнения                         
+  extensions = [
+    basicSetup,
+    autocompletion({ defaultKeymap: false }),
+    Prec.highest(keymap.of([
+      { key: 'Tab', run: acceptCompletion },
+      { key: 'Escape', run: closeCompletion },
+      { key: 'ArrowDown', run: moveCompletionSelection(true) },
+      { key: 'ArrowUp', run: moveCompletionSelection(false) },
+      { key: 'PageDown', run: moveCompletionSelection(true, "page") },
+      { key: 'PageUp', run: moveCompletionSelection(false, "page") },
+      indentWithTab,
+    ]))]
 }
